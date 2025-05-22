6c1dd899229318c64e45be32caba4e003579e2cc
𝙱.𝙼.𝙱-𝚇𝙼𝙳 <hanchobmb@gmail.com>
2025-05-22 18:18:54 +0300

    Add files via upload

--- plugins/download.js
+++ plugins/download.js
@@ -254,7 +254,7 @@ cmd({
 ┃ 📅 *Updated On:* ${app.updated}
 ┃ 👨‍💻 *Developer:* ${app.developer.name}
 ╰━━━━━━━━━━━━━━━┈⊷
-🔗 *Powered By KhanX-AI*`;
+🔗 *Powered By 𝙱.𝙼.𝙱-𝚇𝙼𝙳*`;
 
     await conn.sendMessage(from, { react: { text: "⬆️", key: m.key } });
 
