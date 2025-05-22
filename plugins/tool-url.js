7a61c14b5e8ea4ddf822d65d376dfcd2db858f24
𝙱.𝙼.𝙱-𝚇𝙼𝙳 <hanchobmb@gmail.com>
2025-05-22 08:32:31 +0300

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by NEXUS TECH ☣️`
+      `> © Uploaded by 𝐁.𝐌.𝐁-𝐗𝐌𝐃 ⏳`
     );
 
   } catch (error) {
