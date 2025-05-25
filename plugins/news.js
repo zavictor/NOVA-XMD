4cb8f06ed8855c9bd26fc5dbe6ea7f041e32970d
𝙱.𝙼.𝙱-𝚇𝙼𝙳 <hanchobmb@gmail.com>
2025-05-25 23:01:08 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Nexus Tech
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙱.𝙼.𝙱-𝚇𝙼𝙳
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
