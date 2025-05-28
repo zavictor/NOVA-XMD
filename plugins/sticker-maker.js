5b156eb2db518b4698bc70466c124a4c4d0b0cb3
𝙱.𝙼.𝙱-𝚇𝙼𝙳 <hanchobmb@gmail.com>
2025-05-29 01:07:08 +0300

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "Pkdriller";
+        let pack = Config.STICKER_NAME || "bmn tech";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
