b22df9d4a3cd8b512d35cce07c6894fc37df5608
𝙱.𝙼.𝙱-𝚇𝙼𝙳 <hanchobmb@gmail.com>
2025-06-19 05:31:26 +0300

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,64 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require('os');
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
+
+cmd({
+    pattern: "bmb11",
+    alias: ["menup"],
+    desc: "Auto Generated Full Menu",
+    category: "menu",
+    react: "🧾",
+    filename: __filename
+}, async (conn, mek, m, { from, sender, pushname, reply }) => {
+    try {
+        const grouped = {};
+
+        for (const command of commands) {
+            const cat = command.category?.toUpperCase() || "OTHER";
+            if (!grouped[cat]) grouped[cat] = [];
+            grouped[cat].push(command.pattern);
+        }
+
+        let menutext = `╭━━〔 *🌐 ${config.BOT_NAME || 'B.M.B-XMD'} AUTO MENU* 〕━━┈⊷\n`;
+        menutext += `┃👤 *Owner:* ${config.OWNER_NAME}\n┃📟 *Mode:* ${config.MODE}\n┃📚 *Prefix:* ${config.PREFIX}\n┃⚙️ *Platform:* VPS\n┃⏱️ *Runtime:* ${runtime(process.uptime())}\n╰━━━━━━━━━━━━━━━⊷\n`;
+
+        for (const [category, cmds] of Object.entries(grouped)) {
+            menutext += `\n╭━━〔 *${category}* 〕━━┈⊷\n┃◈╭─────────────·๏\n`;
+            cmds.sort().forEach(cmd => {
+                menutext += `┃◈┃• ${cmd}\n`;
+            });
+            menutext += `┃◈└────────────┈⊷\n╰──────────────┈⊷\n`;
+        }
+
+        await conn.sendMessage(
+            from,
+            {
+                image: { url: 'https://github.com/bmb200/BMB-DATA/raw/refs/heads/main/image/menu1.jpg' },
+                caption: menutext,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 1000,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363382023564830@newsletter',
+                        newsletterName: '🌐𝐁.𝐌.𝐁-𝐗𝐌𝐃🌐',
+                        serverMessageId: 222
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+        await conn.sendMessage(from, {
+            audio: { url: 'https://github.com/bmb200/BMB-DATA/raw/refs/heads/main/media/menu1.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply(`❌ Error:\n${e}`);
+    }
+});
