d475ec7e6b49a4cf535051dab411a6a4b189fb25
𝙱.𝙼.𝙱-𝚇𝙼𝙳 <hanchobmb@gmail.com>
2025-06-22 19:08:02 +0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:lts-buster
-RUN git clone https://github.com/bmb200/B.M.B-XMD/root/ikJawad
-WORKDIR /root/ikJawad
+RUN git clone https://github.com/bmbtech11/NOVA-XMD/root/popkidxmd
+WORKDIR /root/bmbtech11
 RUN npm install && npm install -g pm2 || yarn install --network-concurrency 1
 COPY . .
 EXPOSE 9090
--- config.js
+++ config.js
@@ -5,41 +5,83 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "POPKID~QL0FwBoS#K_SYm4UZHHHCNN8r9EeOv8nLBaGr3vKpMB6z9S5W4Mc",
-  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY B.M.B-XMD 🤍*",
-  MENTION_REPLY: process.env.MENTION_REPLY || "false",
-  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/pz83rc.jpg",
-  PREFIX: process.env.PREFIX || ".",
-  BOT_NAME: process.env.BOT_NAME || "B.M.B-XMD",
-  STICKER_NAME: process.env.STICKER_NAME || "B.M.B-XMD",
-  CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-  CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-  DELETE_LINKS: process.env.DELETE_LINKS || "false",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255767862457",
-  OWNER_NAME: process.env.OWNER_NAME || "𝙱.𝙼.𝙱-𝚇𝙼𝙳",
-  DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙱.𝙼.𝙱-𝚇𝙼𝙳",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/pz83rc.jpg",
-  LIVE_MSG: process.env.LIVE_MSG || "> Am Active *B.M.B-XMD*⚡",
-  READ_MESSAGE: process.env.READ_MESSAGE || "false",
-  AUTO_REACT: process.env.AUTO_REACT || "false",
-  ANTI_BAD: process.env.ANTI_BAD || "false",
-  MODE: process.env.MODE || "public",
-  ANTI_LINK: process.env.ANTI_LINK || "true",
-  AUTO_VOICE: process.env.AUTO_VOICE || "false",
-  AUTO_STICKER: process.env.AUTO_STICKER || "false",
-  AUTO_REPLY: process.env.AUTO_REPLY || "false",
-  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-  PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-  AUTO_TYPING: process.env.AUTO_TYPING || "false",
-  READ_CMD: process.env.READ_CMD || "false",
-  DEV: process.env.DEV || "255767862457",
-  ANTI_VV: process.env.ANTI_VV || "true",
-  ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log",
-  AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
-  ANTICALL: process.env.ANTICALL || "true",
-  CHATBOT: process.env.CHATBOT || "false"
-};
+SESSION_ID: process.env.SESSION_ID || "POPKID;;;TIZS1bRB#tdEuL2kc47iAhItPKKQ_XaqkKLXp8zcqfR9_BoFxsQ8",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY 𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 🤍*",
+// set the auto reply massage on status reply  
+WELCOME: process.env.WELCOME || "true",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/p0xeh3.jpg",
+// add custom menu and mention reply image url
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "NOVA-XMD",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "NOVA-XMD",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255767862457",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "𝙽𝙾𝚅𝙰-𝚇𝙼𝙳",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© powered by nova xmd*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/p0xeh3.jpg",
+// add img for alive msg
+LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *NOVA-XMD*⚡",
+// add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+// make anti link true,false for groups 
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+// make true for send automatic voices
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "NOVA-XMD",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+// change it to 'same' if you want to resend deleted message in same chat 
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+// make it true for auto recoding
+ANTICALL: process.env.ANTICALL || "false",
+// make it true for auto recoding 
+OPENAI_API_KEY: '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'
+//other
+};    
     
